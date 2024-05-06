f767e1af2e6fc261ea52508106b2286f9af8d0d0
licedzindzik <alicedzindzik8@gmail.com>
2024-05-06 15:18:00 -0300

    new file:   ContaBanco/.vscode/settings.json 	new file:   ContaBanco/README.md 	new file:   ContaBanco/src/contaTerminal.java

---
+++ ContaBanco/src/contaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class contaTerminal {
+    public static void main (String [] args) {
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Por favor, insira seu nome: ");
+        String nomeCliente = read.next();
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int numeroConta = read.nextInt();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        int numeroAgencia = read.nextInt();
+
+        System.out.println("Por favor, deposite um valor: ");
+        double saldoConta = read.nextDouble();
+
+        System.out.println("Olá ".concat(nomeCliente)+", obrigado por criar uma conta em nosso banco! Sua agência é: "+ numeroAgencia + ", conta de número: " + numeroConta + ", e seu saldo de R$" + saldoConta + " reais já está disponivel para saque.");
+        } 
+        
+    }
