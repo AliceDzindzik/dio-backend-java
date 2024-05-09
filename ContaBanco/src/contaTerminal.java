fb2da356af7c0195f1320f68c583bbcb02de6580
licedzindzik <alicedzindzik8@gmail.com>
2024-05-09 16:53:58 -0300

    new file:   CaixaEletronico/.vscode/settings.json 	new file:   CaixaEletronico/README.md 	new file:   CaixaEletronico/src/CaixaEletronico.java 	modified:   ContaBanco/src/contaTerminal.java

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -4,7 +4,7 @@ public class contaTerminal {
     public static void main (String [] args) {
         Scanner read = new Scanner(System.in);
 
-        System.out.println("Por favor, insira seu nome: ");
+        System.out.println("Por favor, insira seu nome e sobrenome: ");
         String nomeCliente = read.next();
 
         System.out.println("Por favor, digite o número da conta: ");
@@ -16,7 +16,9 @@ public class contaTerminal {
         System.out.println("Por favor, deposite um valor: ");
         double saldoConta = read.nextDouble();
 
-        System.out.println("Olá ".concat(nomeCliente)+", obrigado por criar uma conta em nosso banco! Sua agência é: "+ numeroAgencia + ", conta de número: " + numeroConta + ", e seu saldo de R$" + saldoConta + " reais já está disponivel para saque.");
-        } 
-        
-    }
+        System.out.println("Olá ".concat(nomeCliente.toUpperCase())+", obrigado(a) por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é: "+ numeroAgencia + ", conta de número: " + numeroConta + ", e seu saldo de R$" + saldoConta + " reais já está disponivel para saque.");
+        read.close();
+
+    } 
+}
